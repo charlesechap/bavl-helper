5fb5da61fa4be6e55c035731153efbfa08ed769c
Charles Echap <charles.echap@gmail.com>
2026-03-05 13:15:24 +0100

    fix: canard traverse l'écran en 1.5s au lieu de 3s

--- bavl-helper/DuckView.swift
+++ bavl-helper/DuckView.swift
@@ -154,8 +154,8 @@ struct DuckHeaderView: View {
                 // vitesse cible : 0.18s/cycle → screenWidth / (0.18/2) cycles en vol
                 // On calcule le step pour traverser screenWidth en ~3s (durée totale)
                 let cycleTime: Double = 0.18
-                let totalTime: Double = 3.0
-                let nCycles           = totalTime / cycleTime        // ~16.7
+                let totalTime: Double = 1.5
+                let nCycles           = totalTime / cycleTime        // ~8.3
                 let step              = screenWidth / CGFloat(nCycles)
 
                 func tick() {
